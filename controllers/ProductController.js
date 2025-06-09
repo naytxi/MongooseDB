01d0762693e2436dd1b3421657308606d5bfeae9
Nahia Otegui <nay.fullstack@gmail.com>
2025-06-09 20:12:41 +0200

    añado modelo, controlador y rutas de productos para base de datos mongobd

---
+++ controllers/ProductController.js
@@ -0,0 +1,75 @@
+const Product = require('../models/Product')
+
+const ProductController = {
+ async create(req, res) {
+   try {
+     const product = await Product.create(req.body)
+     res.status(201).send(product)
+   } catch (error) {
+     console.error(error)
+     res
+       .status(500)
+       .send({ message: 'Ha habido un problema al crear el producto' })
+   }
+ },
+
+  async getAll(req, res) {
+   try {
+     const products = await Product.find()
+     res.send(products)
+   } catch (error) {
+     console.error(error)
+   }
+ },
+
+ async getById(req, res) {
+   try {
+     const product = await Product.findById(req.params._id)
+     res.send(product)
+   } catch (error) {
+     console.error(error)
+   }
+ },
+ 
+ async getProductsByName(req, res) {
+   try {
+     /*const name = new RegExp(req.params.name, 'i')
+     const products = await Product.find({ name })*/
+     const products = await Product.find({
+       $text: {
+         $search: req.params.name,
+       },
+     })
+     res.send(products)
+   } catch (error) {
+     console.log(error)
+   }
+ },
+
+ async delete(req, res) {
+   try {
+     const product = await Product.findByIdAndDelete(req.params._id)
+     res.send({ product, message: 'Product deleted' })
+   } catch (error) {
+     console.error(error)
+     res.status(500).send({
+         message: 'there was a problem trying to remove the product',
+       })
+   }
+},
+
+ async update(req, res) {
+   try {
+     const product = await Product.findByIdAndUpdate(
+       req.params._id,
+       req.body,
+       { new: true }
+     )
+     res.send({ message: 'product successfully updated', product })
+   } catch (error) {
+     console.error(error)
+   }
+ },
+
+}
+module.exports = ProductController
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const { dbConnection } = require('./config/config')
+
+app.use(express.json())
+dbConnection()
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
+
+app.use ('/products', require('./routes/products'))
+
